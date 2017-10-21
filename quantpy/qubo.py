08b7bc7a6a39394a17be8f975dc50b4a8ad43ba4
K.YAMAZAKI | OpenQL Project <kyamaz@openql.org>
2017-10-22 03:00:15 +0900

    fixed indent issues in qubo.py

--- quantpy/qubo.py
+++ quantpy/qubo.py
@@ -2,11 +2,11 @@
 import numpy as np
 
 def getMat(qubo):
-	N = len(qubo)
+        N = len(qubo)
         h = []
         J = [[0]*N for i in range(N)]
-	Jsum = 0
-	hsum = 0
+        Jsum = 0
+        hsum = 0
 
         for j in range(N):
                 sum = 0
@@ -16,13 +16,13 @@ def getMat(qubo):
                         sum += r
                         J[j][i] = r*1.0/4
                         J[i][j] = r*1.0/4
-			Jsum += r*1.0/4
+                        Jsum += r*1.0/4
 
-		for i in range(0,j):
-			sum += qubo[j][i]
+                for i in range(0,j):
+                        sum += qubo[j][i]
 
-		s = qubo[j][j]
-		hsum += s*1.0/2
+                s = qubo[j][j]
+                hsum += s*1.0/2
                 h.append(s*1.0/2 + sum)
 
         return np.array([h,J,Jsum+hsum])
