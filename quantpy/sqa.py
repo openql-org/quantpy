122c5300bd6daaeee234c48745aa7c506c93721b
K.YAMAZAKI | OpenQL Project <kyamaz@openql.org>
2017-10-22 03:20:07 +0900

    merge mdrft's updates and the latest feature-hokusai (#5)
    mdrft's updates are:
    
    * update repeat calc 
    * update for repeat of trial and comments
    * update qubo and sqa separated
    * update cost function
    * updated some comments
    * bug fixed
    
    kyamaz's updates are:
    
    * added some files for travisCI
    * updated quantpy/test.py to perform travisCI
    * updated some files to perform test on travisCI
    * merged openql-org/feature-hokusai
    * updated test_sqa.py for 1 passed
    * updated .travis.yml that deleted target of Python:3.7
    * fixed indent issues in qubo.py

--- quantpy/sqa.py
+++ quantpy/sqa.py
@@ -1,9 +1,13 @@
 from numpy import *
 
 #run sqa algorithm with params
-def run(N,kT,G,m,h,J,c,rep):
+def run(kT,Ginit,m,mat,rep):
 
-	tau = 0.9
+	h = mat[0]
+	J = mat[1]
+	c = mat[2]
+	N = len(h)
+	tau = 0.99
 	Gfin = 0.01
 	qarr = []
 	Earr = []
@@ -11,16 +15,18 @@ def run(N,kT,G,m,h,J,c,rep):
 	# simulated quantum annealing simulator using quantum monte carlo & metropolis
 
 	for j in range(rep):
+		G = Ginit
+
 		q = []
-		for i in range(N):
+		for i in range(m):
 			q.append(random.choice([-1,1],N))
 
 		while G > Gfin:
 			for i in range(N*m):
 				x = random.randint(N)
 				y = random.randint(m)
-				dE = (2*q[y][x]*(h[x]+q[y][(N+x-1)%N]*J[x][(N+x-1)%N]+q[y][(x+1)%N]*J[x][(x+1)%N]))/m
-				dE += q[y][x]*(q[(m+y-1)%m][x]+q[(y+1)%m][x])*log(1/tanh(G/kT/m))/kT
+				dE = (2*q[y][x]*(h[x]+q[y][(N+x-1)%N]*J[x][(N+x-1)%N]+q[y][(x+1)%N]*J[x][(x+1)%N]))*1.0/m
+				dE += -q[y][x]*(q[(m+y-1)%m][x]+q[(y+1)%m][x])*log(tanh(G/kT/m))*1.0/kT
 				if exp(-dE/kT)>random.rand():
 					q[y][x] = -q[y][x]
 			G*=tau
@@ -30,7 +36,6 @@ def run(N,kT,G,m,h,J,c,rep):
 			E += h[a]*q[0][a]
 			for b in range(a+1,N):
 				E += J[a][b]*q[0][a]*q[0][b]
-
 		qarr.append(q)
 		Earr.append(E+c)
 
