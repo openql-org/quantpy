f67e5dc609f1712afa46a8503a8365a9f2791987
Kiyohito Yamazaki | OpenQL Project <kyamaz@openql.org>
2017-10-21 20:35:43 +0900

    updated some files to perform test on travisCI

---
+++ quantpy/conftest.py
@@ -0,0 +1,7 @@
+from __future__ import print_function, division
+
+import sys
+sys._running_pytest = True
+from distutils.version import LooseVersion as V
+
+import pytest
--- quantpy/tests/test_sqa.py
+++ quantpy/tests/test_sqa.py
@@ -1,4 +1,5 @@
+# -*- coding: utf-8 -*-
+from quantpy import sqa
 import numpy as np
-import quantpy.sqa as sqa
 
 sqa.run(4,0.02,3,0.02,3,np.array([[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]]),0.6)
