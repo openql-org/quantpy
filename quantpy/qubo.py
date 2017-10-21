4db7a3f520e767dfa19f4e3c72840b21529ff8ee
K.YAMAZAKI | OpenQL Project <kyamaz@openql.org>
2017-10-22 03:20:07 +0900

    merge mdrft's updates and the latest feature-hokusai (#5)
    mdrft's updates are:
    
    * update repeat calc 
    * update for repeat of trial and comments
    * update qubo and sqa separated
    * update cost function
    * updated some comments
    * bug fixed
    
    kyamaz's updates are:
    
    * added some files for travisCI
    * updated quantpy/test.py to perform travisCI
    * updated some files to perform test on travisCI
    * merged openql-org/feature-hokusai
    * updated test_sqa.py for 1 passed
    * updated .travis.yml that deleted target of Python:3.7
    * fixed indent issues in qubo.py

--- quantpy/qubo.py
+++ quantpy/qubo.py
@@ -1,30 +1,28 @@
-#get h from qubo
-def geth(N,qubo):
+#get ising matrix from qubo
+import numpy as np
+
+def getMat(qubo):
+        N = len(qubo)
         h = []
-        for j in range(N):
-                 Jsum = 0
-                 for i in range(j+1,N):
-                        Jsum += qubo[j][i] 
-                 h.append(qubo[j][j]*1.0/2 + Jsum)
-        return h
-
-#get Jij from qubo
-def getj(N,qubo):
         J = [[0]*N for i in range(N)]
-        for j in range(N):
-                for i in range(j+1,N):
-                        r = qubo[j][i]
-                        J[j][i] = r*1.0/4
-                        J[i][j] = r*1.0/4
-
-        return J
-
-#get constant term from qubo
-def getc(N,qubo):
         Jsum = 0
         hsum = 0
+
         for j in range(N):
-          hsum += qubo[j][j]*1.0/2
-          for i in range(j+1,N):
-            Jsum += qubo[j][i]*1.0/4
-        return Jsum+hsum 
+                sum = 0
+
+                for i in range(j+1,N):
+                        r = qubo[j][i]
+                        sum += r
+                        J[j][i] = r*1.0/4
+                        J[i][j] = r*1.0/4
+                        Jsum += r*1.0/4
+
+                for i in range(0,j):
+                        sum += qubo[j][i]
+
+                s = qubo[j][j]
+                hsum += s*1.0/2
+                h.append(s*1.0/2 + sum)
+
+        return np.array([h,J,Jsum+hsum])
--- quantpy/tests/test_sqa.py
+++ quantpy/tests/test_sqa.py
@@ -2,7 +2,7 @@
 from quantpy import sqa, qubo
 import numpy as np
 
-#params are number of qubits, temperature, Gamma, trotter number, qubo matrix, number of trials
+#params are temperature, Gamma, trotter number, qubo matrix, number of trials
 def test_qubo_sqa():
-  mat = np.array([[0,1,1,1],[1,0,1,1],[1,1,0,1],[1,1,1,0]])
-  sqa.run(4,0.02,3,3,qubo.geth(4,mat),qubo.getj(4,mat),qubo.getc(4,mat),10)
+  mat = np.array([[-32,4,4,4,4,4,4,4],[4,-32,4,4,4,4,4,4],[4,4,-32,4,4,4,4,4],[4,4,4,-32,4,4,4,4],[4,4,4,4,-32,4,4,4],[4,4,4,4,4,-32,4,4],[4,4,4,4,4,4,-32,4],[4,4,4,4,4,4,4,-32]])
+  sqa.run(0.02,5,4,qubo.getMat(mat),10)
