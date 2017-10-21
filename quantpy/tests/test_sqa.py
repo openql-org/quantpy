e27cbb3e97994d691e47664fdc9b4d1b48314c1e
Kiyohito Yamazaki | OpenQL Project <kyamaz@openql.org>
2017-10-21 22:04:38 +0900

    updated test_sqa.py for 1 passed

--- quantpy/tests/test_sqa.py
+++ quantpy/tests/test_sqa.py
@@ -3,5 +3,6 @@ from quantpy import sqa, qubo
 import numpy as np
 
 #params are number of qubits, temperature, Gamma, trotter number, qubo matrix, number of trials
-mat = np.array([[0,1,1,1],[1,0,1,1],[1,1,0,1],[1,1,1,0]])
-sqa.run(4,0.02,3,3,qubo.geth(4,mat),qubo.getj(4,mat),qubo.getc(4,mat),10)
+def test_qubo_sqa():
+  mat = np.array([[0,1,1,1],[1,0,1,1],[1,1,0,1],[1,1,1,0]])
+  sqa.run(4,0.02,3,3,qubo.geth(4,mat),qubo.getj(4,mat),qubo.getc(4,mat),10)
