4ece9ad07a564f361a6bbdd353556194f677bdd8
mdr_minato <minato@mdrft.com>
2017-10-21 18:45:01 +0900

    update for repeat of trial and comments

--- quantpy/test.py
+++ quantpy/test.py
@@ -1,4 +1,5 @@
 import numpy as np
 import sqa
 
+#params are N = number of qubits, kT = temperature, Gamma, trotter number, qubo matrix, number of trial and result
 sqa.run(4,0.02,3,3,np.array([[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]]),10)
